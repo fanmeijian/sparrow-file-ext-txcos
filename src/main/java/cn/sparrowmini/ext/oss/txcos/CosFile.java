78158e61552c67b0c0211c434ffd6434fa30936d
Fan Sword <fanmeijian@gmail.com>
2025-01-02 12:46:39 +0800

    适应新版本SPARROW ORG

--- src/main/java/cn/sparrowmini/ext/oss/txcos/CosFile.java
+++ src/main/java/cn/sparrowmini/ext/oss/txcos/CosFile.java
@@ -2,13 +2,13 @@ package cn.sparrowmini.ext.oss.txcos;
 
 import cn.sparrowmini.file.model.BaseCosFile;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Set;
 
@@ -22,7 +22,11 @@ public class CosFile extends BaseCosFile implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+	@Id
+	@GenericGenerator(name = "id-generator", strategy = "uuid")
+	@GeneratedValue(generator = "id-generator")
+	private String id;
 
 	@ElementCollection
 	private Set<String> catalog;
