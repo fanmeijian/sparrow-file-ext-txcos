bb51bf3b79bd3e8600337d596e86f534c8baa36c
Fan Sword <fanmeijian@gmail.com>
2025-07-29 19:51:37 +0800

    完善腾讯上传文件功能

--- src/main/java/cn/sparrowmini/ext/oss/txcos/ObjectStorageService.java
+++ src/main/java/cn/sparrowmini/ext/oss/txcos/ObjectStorageService.java
@@ -1,48 +1,25 @@
 package cn.sparrowmini.ext.oss.txcos;
 
-import cn.sparrowmini.file.model.BaseCosFile;
-import cn.sparrowmini.file.model.CosFile;
+
+import com.tencent.cloud.Response;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.tencent.cloud.Response;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "cos")
 @RequestMapping(value = "cos/tx")
 public interface ObjectStorageService {
 	@GetMapping(value = "/uploadTmpKeys", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public Response getUploadTmpKey(@Nullable @RequestParam String fileName,@Nullable @RequestParam String path);
+	public Response getUploadTmpKey(String fileName, String path);
 
 	@GetMapping(value = "/downloadTmpKeys", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public Response getDownloadTmpKey(@RequestParam String fileName, @RequestParam String path);
+	public Response getDownloadTmpKey(String fileName, String path);
 
-	@PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	@Operation(summary = "上传文件到TXCOS", operationId = "uploadToTxCos")
-	@ResponseBody
-	public CosFile upload(@RequestParam MultipartFile file);
-	
-	@PostMapping(value = "/cos-files")
-	@Operation(summary = "创建cosfile对象", operationId = "createCosFile")
-	@ResponseBody
-	public CosFile createFile(@RequestBody CosFile file);
-
-	@GetMapping(value = "/{fileId}/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-	@Operation(summary = "从TXCOS下载文件", operationId = "download")
-	@ResponseBody
-	public byte[] download(@PathVariable String fileId);
-
-	public byte[] download(BaseCosFile cosFile);
 }
--- src/main/java/cn/sparrowmini/ext/oss/txcos/ObjectStorageServiceImpl.java
+++ src/main/java/cn/sparrowmini/ext/oss/txcos/ObjectStorageServiceImpl.java
@@ -1,37 +1,20 @@
 package cn.sparrowmini.ext.oss.txcos;
 
-import cn.sparrowmini.file.model.BaseCosFile;
-import cn.sparrowmini.file.model.CosFile;
-import cn.sparrowmini.file.repository.CosFileRepository;
-import cn.sparrowmini.file.service.DownloadPermission;
-import com.qcloud.cos.COSClient;
-import com.qcloud.cos.ClientConfig;
-import com.qcloud.cos.auth.BasicCOSCredentials;
-import com.qcloud.cos.auth.COSCredentials;
-import com.qcloud.cos.exception.CosClientException;
-import com.qcloud.cos.http.HttpProtocol;
-import com.qcloud.cos.model.*;
-import com.qcloud.cos.region.Region;
-import com.qcloud.cos.utils.IOUtils;
+import cn.sparrowmini.common.service.DownloadPermission;
 import com.tencent.cloud.CosStsClient;
 import com.tencent.cloud.Response;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.transaction.Transactional;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.TreeMap;
 
 @Slf4j
 @Service
+@RestController
 public class ObjectStorageServiceImpl implements ObjectStorageService {
 
     @Autowired
@@ -44,8 +27,6 @@ public class ObjectStorageServiceImpl implements ObjectStorageService {
     @Autowired
     private HttpServletResponse response;
 
-    @Autowired
-    private CosFileRepository cosFileRepository;
 
 
     @Override
@@ -114,161 +95,6 @@ public class ObjectStorageServiceImpl implements ObjectStorageService {
         } catch (Exception e) {
             throw new IllegalArgumentException("no valid secret !");
         }
-    }
-
-    @Transactional
-    @Override
-    public CosFile upload(MultipartFile file) {
-
-        // 调用 COS 接口之前必须保证本进程存在一个 COSClient 实例，如果没有则创建
-        // 详细代码参见本页：简单操作 -> 创建 COSClient
-        COSClient cosClient = createCOSClient();
-
-        // 存储桶的命名格式为 BucketName-APPID，此处填写的存储桶名称必须为此格式
-        String bucketName = this.config.getBucket();
-        // 对象键(Key)是对象在存储桶中的唯一标识。
-
-        // 这里创建一个 ByteArrayInputStream 来作为示例，实际中这里应该是您要上传的 InputStream 类型的流
-
-        InputStream inputStream;
-        try {
-            byte data[] = file.getBytes();
-            String key = DigestUtils.md5Hex(data).toUpperCase();
-
-            CosFile cosFile = new CosFile();
-            cosFile.setBucket(this.config.getBucket());
-            cosFile.setRegion(this.config.getRegion());
-            cosFile.setName(key);
-            cosFile.setFileName(file.getOriginalFilename());
-
-            this.cosFileRepository.save(cosFile);
-            inputStream = new ByteArrayInputStream(data);
-            ObjectMetadata objectMetadata = new ObjectMetadata();
-            // 上传的流如果能够获取准确的流长度，则推荐一定填写 content-length
-            // 如果确实没办法获取到，则下面这行可以省略，但同时高级接口也没办法使用分块上传了
-            objectMetadata.setContentLength(inputStream.available());
-
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, inputStream, objectMetadata);
-
-            // 设置存储类型（如有需要，不需要请忽略此行代码）, 默认是标准(Standard), 低频(standard_ia)
-            // 更多存储类型请参见 https://cloud.tencent.com/document/product/436/33417
-            putObjectRequest.setStorageClass(StorageClass.Standard_IA);
-            PutObjectResult putObjectResult = cosClient.putObject(putObjectRequest);
-
-            cosFile.setHash(putObjectResult.getContentMd5());
-            String url = httpServletRequest.getRequestURL().toString().replace(httpServletRequest.getServletPath(), "");
-            this.cosFileRepository.save(cosFile);
-
-            return cosFile;
-        } catch (IOException |CosClientException e) {
-            log.error(e.getMessage(), e);
-        }
-        // 确认本进程不再使用 cosClient 实例之后，关闭即可
-        cosClient.shutdown();
-        return null;
-
-    }
-
-    // 创建 COSClient 实例，这个实例用来后续调用请求
-    private COSClient createCOSClient() {
-        // 设置用户身份信息。
-        // SECRETID 和 SECRETKEY 请登录访问管理控制台 https://console.cloud.tencent.com/cam/capi
-        // 进行查看和管理
-        String secretId = this.config.getSecretId();// 用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见
-        // https://cloud.tencent.com/document/product/598/37140
-        String secretKey = this.config.getSecretKey();// 用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见
-        // https://cloud.tencent.com/document/product/598/37140
-        COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
-
-        // ClientConfig 中包含了后续请求 COS 的客户端设置：
-        ClientConfig clientConfig = new ClientConfig();
-
-        // 设置 bucket 的地域
-        // COS_REGION 请参见 https://cloud.tencent.com/document/product/436/6224
-        clientConfig.setRegion(new Region(this.config.getRegion()));
-
-        // 设置请求协议, http 或者 https
-        // 5.6.53 及更低的版本，建议设置使用 https 协议
-        // 5.6.54 及更高版本，默认使用了 https
-        clientConfig.setHttpProtocol(HttpProtocol.https);
-
-        // 以下的设置，是可选的：
-
-        // 设置 socket 读取超时，默认 30s
-        clientConfig.setSocketTimeout(30 * 1000);
-        // 设置建立连接超时，默认 30s
-        clientConfig.setConnectionTimeout(30 * 1000);
-
-        // 如果需要的话，设置 http 代理，ip 以及 port
-//		clientConfig.setHttpProxyIp("httpProxyIp");
-//		clientConfig.setHttpProxyPort(80);
-
-        // 生成 cos 客户端。
-        return new COSClient(cred, clientConfig);
-    }
-
-    @Override
-    public byte[] download(String fileId) {
-        CosFile cosFile = this.cosFileRepository.findById(fileId).get();
-        // 调用 COS 接口之前必须保证本进程存在一个 COSClient 实例，如果没有则创建
-        // 详细代码参见本页：简单操作 -> 创建 COSClient
-
-        return this.download(cosFile);
-    }
-
-    @Override
-    public CosFile createFile(CosFile file) {
-        return this.cosFileRepository.save(file);
-    }
-
-
-    @Override
-    public byte[] download(BaseCosFile cosFile) {
-        COSClient cosClient = createCOSClient();
-
-        // 存储桶的命名格式为 BucketName-APPID，此处填写的存储桶名称必须为此格式
-        String bucketName = cosFile.getBucket();
-        // 对象键(Key)是对象在存储桶中的唯一标识。详情请参见
-        // [对象键](https://cloud.tencent.com/document/product/436/13324)
-        String key = cosFile.getName();
-
-        GetObjectRequest getObjectRequest = new GetObjectRequest(bucketName, key);
-        InputStream cosObjectInput = null;
-
-        try {
-            COSObject cosObject = cosClient.getObject(getObjectRequest);
-            cosObjectInput = cosObject.getObjectContent();
-        } catch (CosClientException e) {
-            log.error(e.getMessage(), e);
-        }
-
-        // 处理下载到的流
-        // 这里是直接读取，按实际情况来处理
-        byte[] bytes = null;
-        try {
-            bytes = IOUtils.toByteArray(cosObjectInput);
-            // 在流没有处理完之前，不能关闭 cosClient
-            // 确认本进程不再使用 cosClient 实例之后，关闭即可
-            cosClient.shutdown();
-
-            response.setHeader("Content-Disposition", "attachment; filename=\"" + cosFile.getFileName() + "\"");
-
-            return bytes;
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
-        } finally {
-            // 用完流之后一定要调用 close()
-            try {
-                cosObjectInput.close();
-            } catch (IOException e) {
-               log.error(e.getMessage(), e);
-            }
-        }
-
-        // 在流没有处理完之前，不能关闭 cosClient
-        // 确认本进程不再使用 cosClient 实例之后，关闭即可
-        cosClient.shutdown();
-        return bytes;
     }
 
 }
